5332fbaf51e7e8691cf40a11e6f533964bde631b
Yasasvi56 <61499237+Yasasvi56@users.noreply.github.com>
2021-02-20 13:40:55 +0530

    Update Hello.java

--- FirstProject/src/Hello.java
+++ FirstProject/src/Hello.java
@@ -2,6 +2,6 @@
 public class Hello {
 	public static void main(String[] args) {
         System.out.println("Hello world");
-	System.out.println("Hello Hello2");
+	System.out.println("Hello Hello3");
     }
 }
